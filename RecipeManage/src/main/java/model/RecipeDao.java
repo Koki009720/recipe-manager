65b63703f9e65f2cbe242653c0039c9e4116d53b
Koki009720 <tanakakouki419@gmail.com>
2026-06-23 17:58:58 +0900

    三日目成果

--- RecipeManage/src/main/java/model/RecipeDao.java
+++ RecipeManage/src/main/java/model/RecipeDao.java
@@ -7,192 +7,232 @@ import java.util.ArrayList;
 
 public class RecipeDao {
 
-    // レシピを追加
-	public void insertRecipe(String recipeName, String url, String memo, String materials, String steps, String thumbnailUrl) throws Exception {
+	// レシピを追加
+	public void insertRecipe(String recipeName, String url, String memo, String materials, String steps,
+			String thumbnailUrl) throws Exception {
 
-    	String sql = "INSERT INTO recipes "
-    	        + "(recipe_name, url, memo, materials, steps, thumbnail_url) "
-    	        + "VALUES (?, ?, ?, ?, ?, ?)";
+		String sql = "INSERT INTO recipes "
+				+ "(recipe_name, url, memo, materials, steps, thumbnail_url) "
+				+ "VALUES (?, ?, ?, ?, ?, ?)";
 
-        Connection con = DbUtil.getConnection();
-        PreparedStatement pstmt = con.prepareStatement(sql);
+		Connection con = DbUtil.getConnection();
+		PreparedStatement pstmt = con.prepareStatement(sql);
 
-        pstmt.setString(1, recipeName);
-        pstmt.setString(2, url);
-        pstmt.setString(3, memo);
-        pstmt.setString(4, materials);
-        pstmt.setString(5, steps);
-        pstmt.setString(6, thumbnailUrl);
+		pstmt.setString(1, recipeName);
+		pstmt.setString(2, url);
+		pstmt.setString(3, memo);
+		pstmt.setString(4, materials);
+		pstmt.setString(5, steps);
+		pstmt.setString(6, thumbnailUrl);
 
-        pstmt.executeUpdate();
+		pstmt.executeUpdate();
 
-        pstmt.close();
-        con.close();
-    }
+		pstmt.close();
+		con.close();
+	}
 
-    // レシピを削除
-    public void deleteRecipe(int id) throws Exception {
+	// レシピを削除
+	public void deleteRecipe(int id) throws Exception {
 
-        String sql = "DELETE FROM recipes WHERE id = ?";
+		String sql = "DELETE FROM recipes WHERE id = ?";
 
-        Connection con = DbUtil.getConnection();
-        PreparedStatement pstmt = con.prepareStatement(sql);
+		Connection con = DbUtil.getConnection();
+		PreparedStatement pstmt = con.prepareStatement(sql);
 
-        pstmt.setInt(1, id);
-        pstmt.executeUpdate();
+		pstmt.setInt(1, id);
+		pstmt.executeUpdate();
 
-        pstmt.close();
-        con.close();
-    }
+		pstmt.close();
+		con.close();
+	}
 
-    // レシピを更新
-    public void updateRecipe(int id, String recipeName, String url, String memo,
-            String materials, String steps, String thumbnailUrl) throws Exception {
+	// レシピを更新
+	public void updateRecipe(int id, String recipeName, String url, String memo,
+			String materials, String steps, String thumbnailUrl) throws Exception {
 
-        String sql = "UPDATE recipes "
-                + "SET recipe_name = ?, url = ?, memo = ?, materials = ?, steps = ?, thumbnail_url = ? "
-                + "WHERE id = ?";
+		String sql = "UPDATE recipes "
+				+ "SET recipe_name = ?, url = ?, memo = ?, materials = ?, steps = ?, thumbnail_url = ? "
+				+ "WHERE id = ?";
 
-        Connection con = DbUtil.getConnection();
-        PreparedStatement pstmt = con.prepareStatement(sql);
+		Connection con = DbUtil.getConnection();
+		PreparedStatement pstmt = con.prepareStatement(sql);
 
-        pstmt.setString(1, recipeName);
-        pstmt.setString(2, url);
-        pstmt.setString(3, memo);
-        pstmt.setString(4, materials);
-        pstmt.setString(5, steps);
-        pstmt.setString(6, thumbnailUrl);
-        pstmt.setInt(7, id);
+		pstmt.setString(1, recipeName);
+		pstmt.setString(2, url);
+		pstmt.setString(3, memo);
+		pstmt.setString(4, materials);
+		pstmt.setString(5, steps);
+		pstmt.setString(6, thumbnailUrl);
+		pstmt.setInt(7, id);
 
-        pstmt.executeUpdate();
+		pstmt.executeUpdate();
 
-        pstmt.close();
-        con.close();
-    }
+		pstmt.close();
+		con.close();
+	}
 
-    // レシピを表示(個別)
-    public Recipe selectRecipeById(int id) throws Exception {
+	// レシピを表示(個別)
+	public Recipe selectRecipeById(int id) throws Exception {
 
-    	String sql = "SELECT id, recipe_name, url, memo, materials, steps, thumbnail_url FROM recipes WHERE id = ?";
+		String sql = "SELECT id, recipe_name, url, memo, materials, steps, thumbnail_url FROM recipes WHERE id = ?";
 
-        Connection con = DbUtil.getConnection();
-        PreparedStatement pstmt = con.prepareStatement(sql);
+		Connection con = DbUtil.getConnection();
+		PreparedStatement pstmt = con.prepareStatement(sql);
 
-        pstmt.setInt(1, id);
+		pstmt.setInt(1, id);
 
-        ResultSet rs = pstmt.executeQuery();
+		ResultSet rs = pstmt.executeQuery();
 
-        Recipe recipe = null;
+		Recipe recipe = null;
 
-        if (rs.next()) {
-            recipe = new Recipe();
-            recipe.setId(rs.getInt("id"));
-            recipe.setRecipeName(rs.getString("recipe_name"));
-            recipe.setUrl(rs.getString("url"));
-            recipe.setMemo(rs.getString("memo"));
-            recipe.setMaterials(rs.getString("materials"));
-            recipe.setSteps(rs.getString("steps"));
-            recipe.setThumbnailUrl(rs.getString("thumbnail_url"));
-        }
+		if (rs.next()) {
+			recipe = new Recipe();
+			recipe.setId(rs.getInt("id"));
+			recipe.setRecipeName(rs.getString("recipe_name"));
+			recipe.setUrl(rs.getString("url"));
+			recipe.setMemo(rs.getString("memo"));
+			recipe.setMaterials(rs.getString("materials"));
+			recipe.setSteps(rs.getString("steps"));
+			recipe.setThumbnailUrl(rs.getString("thumbnail_url"));
+		}
 
-        rs.close();
-        pstmt.close();
-        con.close();
+		rs.close();
+		pstmt.close();
+		con.close();
 
-        return recipe;
-    }
+		return recipe;
+	}
 
-    // レシピを表示(全体)
-    public ArrayList<Recipe> selectAllRecipes() throws Exception {
+	//検索機能
+	public ArrayList<Recipe> searchRecipes(String keyword) throws Exception {
+		ArrayList<Recipe> recipeList = new ArrayList<Recipe>();
 
-        ArrayList<Recipe> recipeList = new ArrayList<Recipe>();
+		String sql = "SELECT id, recipe_name, url, memo, materials, steps, thumbnail_url, favorite "
+				+ "FROM recipes "
+				+ "WHERE recipe_name LIKE ? "
+				+ "OR memo LIKE ? "
+				+ "OR materials LIKE ? "
+				+ "OR steps LIKE ? "
+				+ "ORDER BY id DESC";
 
-        String sql = "SELECT id, recipe_name, url, memo, materials, steps, thumbnail_url, favorite"
-        		+ " FROM recipes ORDER BY id DESC";
+		Connection con = DbUtil.getConnection();
+		PreparedStatement pstmt = con.prepareStatement(sql);
 
-        Connection con = DbUtil.getConnection();
-        PreparedStatement pstmt = con.prepareStatement(sql);
-        ResultSet rs = pstmt.executeQuery();
+		String searchWord = "%" + keyword + "%";
+		pstmt.setString(1, searchWord);
+		pstmt.setString(2, searchWord);
+		pstmt.setString(3, searchWord);
+		pstmt.setString(4, searchWord);
 
-        while (rs.next()) {
-            Recipe recipe = new Recipe();
+		ResultSet rs = pstmt.executeQuery();
 
-            recipe.setId(rs.getInt("id"));
-            recipe.setRecipeName(rs.getString("recipe_name"));
-            recipe.setUrl(rs.getString("url"));
-            recipe.setMemo(rs.getString("memo"));
-            recipe.setMaterials(rs.getString("materials"));
-            recipe.setSteps(rs.getString("steps"));
-            recipe.setThumbnailUrl(rs.getString("thumbnail_url"));
-            recipe.setFavorite(rs.getInt("favorite"));
+		while (rs.next()) {
+			Recipe recipe = new Recipe();
+			recipe.setId(rs.getInt("id"));
+			recipe.setRecipeName(rs.getString("recipe_name"));
+			recipe.setUrl(rs.getString("url"));
+			recipe.setMemo(rs.getString("memo"));
+			recipe.setMaterials(rs.getString("materials"));
+			recipe.setSteps(rs.getString("steps"));
+			recipe.setThumbnailUrl(rs.getString("thumbnail_url"));
+			recipe.setFavorite(rs.getInt("favorite"));
 
-            recipeList.add(recipe);
-        }
+			recipeList.add(recipe);
+		}
 
-        rs.close();
-        pstmt.close();
-        con.close();
+		rs.close();
+		pstmt.close();
+		con.close();
 
-        return recipeList;
-    }
-    //検索機能
-    public ArrayList<Recipe> searchRecipes(String keyword) throws Exception {
-        ArrayList<Recipe> recipeList = new ArrayList<Recipe>();
+		return recipeList;
+	}
 
-        String sql = "SELECT id, recipe_name, url, memo, materials, steps, thumbnail_url, favorite "
-                + "FROM recipes "
-                + "WHERE recipe_name LIKE ? "
-                + "OR memo LIKE ? "
-                + "OR materials LIKE ? "
-                + "OR steps LIKE ? "
-                + "ORDER BY id DESC";
+	// お気に入り切り替え
+	public void updateFavorite(int id, int favorite) throws Exception {
 
-        Connection con = DbUtil.getConnection();
-        PreparedStatement pstmt = con.prepareStatement(sql);
+		String sql = "UPDATE recipes SET favorite = ? WHERE id = ?";
 
-        String searchWord = "%" + keyword + "%";
-        pstmt.setString(1, searchWord);
-        pstmt.setString(2, searchWord);
-        pstmt.setString(3, searchWord);
-        pstmt.setString(4, searchWord);
+		Connection con = DbUtil.getConnection();
+		PreparedStatement pstmt = con.prepareStatement(sql);
 
-        ResultSet rs = pstmt.executeQuery();
+		pstmt.setInt(1, favorite);
+		pstmt.setInt(2, id);
 
-        while (rs.next()) {
-            Recipe recipe = new Recipe();
-            recipe.setId(rs.getInt("id"));
-            recipe.setRecipeName(rs.getString("recipe_name"));
-            recipe.setUrl(rs.getString("url"));
-            recipe.setMemo(rs.getString("memo"));
-            recipe.setMaterials(rs.getString("materials"));
-            recipe.setSteps(rs.getString("steps"));
-            recipe.setThumbnailUrl(rs.getString("thumbnail_url"));
-            recipe.setFavorite(rs.getInt("favorite"));
-            
-            recipeList.add(recipe);
-        }
+		pstmt.executeUpdate();
 
-        rs.close();
-        pstmt.close();
-        con.close();
+		pstmt.close();
+		con.close();
+	}
 
-        return recipeList;
-    }
-    
- // お気に入り切り替え
-    public void updateFavorite(int id, int favorite) throws Exception {
+	// レシピを表示(全体)
+	public ArrayList<Recipe> selectAllRecipes() throws Exception {
 
-        String sql = "UPDATE recipes SET favorite = ? WHERE id = ?";
+		ArrayList<Recipe> recipeList = new ArrayList<Recipe>();
 
-        Connection con = DbUtil.getConnection();
-        PreparedStatement pstmt = con.prepareStatement(sql);
+		String sql = "SELECT id, recipe_name, url, memo, materials, steps, thumbnail_url, favorite"
+				+ " FROM recipes ORDER BY id DESC";
 
-        pstmt.setInt(1, favorite);
-        pstmt.setInt(2, id);
+		Connection con = DbUtil.getConnection();
+		PreparedStatement pstmt = con.prepareStatement(sql);
+		ResultSet rs = pstmt.executeQuery();
 
-        pstmt.executeUpdate();
+		while (rs.next()) {
+			Recipe recipe = new Recipe();
 
-        pstmt.close();
-        con.close();
-    }
+			recipe.setId(rs.getInt("id"));
+			recipe.setRecipeName(rs.getString("recipe_name"));
+			recipe.setUrl(rs.getString("url"));
+			recipe.setMemo(rs.getString("memo"));
+			recipe.setMaterials(rs.getString("materials"));
+			recipe.setSteps(rs.getString("steps"));
+			recipe.setThumbnailUrl(rs.getString("thumbnail_url"));
+			recipe.setFavorite(rs.getInt("favorite"));
+
+			recipeList.add(recipe);
+		}
+
+		rs.close();
+		pstmt.close();
+		con.close();
+
+		return recipeList;
+	}
+
+	//お気に入り一覧表示
+	public ArrayList<Recipe> selectFavoriteRecipes() throws Exception {
+
+		ArrayList<Recipe> recipeList = new ArrayList<Recipe>();
+
+		String sql = "SELECT id, recipe_name, url, memo, materials, steps, thumbnail_url, favorite"
+				+ " FROM recipes"
+				+ " WHERE favorite = 1"
+				+ " ORDER BY id DESC";
+
+		Connection con = DbUtil.getConnection();
+		PreparedStatement pstmt = con.prepareStatement(sql);
+		ResultSet rs = pstmt.executeQuery();
+
+		while (rs.next()) {
+			Recipe recipe = new Recipe();
+
+			recipe.setId(rs.getInt("id"));
+			recipe.setRecipeName(rs.getString("recipe_name"));
+			recipe.setUrl(rs.getString("url"));
+			recipe.setMemo(rs.getString("memo"));
+			recipe.setMaterials(rs.getString("materials"));
+			recipe.setSteps(rs.getString("steps"));
+			recipe.setThumbnailUrl(rs.getString("thumbnail_url"));
+			recipe.setFavorite(rs.getInt("favorite"));
+
+			recipeList.add(recipe);
+		}
+
+		rs.close();
+		pstmt.close();
+		con.close();
+
+		return recipeList;
+
+	}
+
 }
--- RecipeManage/src/main/webapp/RecipeList.jsp
+++ RecipeManage/src/main/webapp/RecipeList.jsp
@@ -6,108 +6,179 @@
 
 <%
 String keyword = request.getParameter("keyword");
+String favoriteOnly = request.getParameter("favoriteOnly");
 
 RecipeDao dao = new RecipeDao();
 ArrayList<Recipe> recipeList;
 
-if (keyword != null && keyword.length() > 0) {
-    recipeList = dao.searchRecipes(keyword);
+if ("1".equals(favoriteOnly)) {
+	recipeList = dao.selectFavoriteRecipes();
+} else if (keyword != null && keyword.length() > 0) {
+	recipeList = dao.searchRecipes(keyword);
 } else {
-    recipeList = dao.selectAllRecipes();
+	recipeList = dao.selectAllRecipes();
 }
 %>
+
+<%!public String addStepNumbers(String steps) {
+		if (steps == null || steps.length() == 0) {
+			return "";
+		}
+
+		String[] lines = steps.split("\\r?\\n");
+		String result = "";
+
+		for (int i = 0; i < lines.length; i++) {
+			result += (i + 1) + ". " + lines[i] + "<br>";
+		}
+
+		return result;
+	}%>
+
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>料理一覧</title>
+<style>
+body {
+	background-color: #f7f3ea;
+	font-family: sans-serif;
+	margin: 30px;
+}
+
+.recipe-card {
+	background-color: white;
+	border-radius: 12px;
+	padding: 20px;
+	margin-bottom: 24px;
+	box-shadow: 0 2px 8px rgba(0, 0, 0, 0.15);
+	max-width: 600px;
+}
+
+.recipe-card img {
+    width: 100%;
+    max-width: 360px;
+    border-radius: 10px;
+    display: block;
+    margin: 20px auto;
+}
+
+button {
+	padding: 8px 14px;
+	margin: 4px;
+	border-radius: 6px;
+	border: none;
+	cursor: pointer;
+}
+
+a {
+	color: #d2691e;
+}
+
+
+</style>
 </head>
 <body>
-<form action="RecipeList.jsp" method="get">
-    <input type="text" name="keyword" value="<%= keyword == null ? "" : keyword %>">
-    <button type="submit">検索</button>
-</form>
+
+	<form action="RecipeList.jsp" method="get">
+		<input type="text" name="keyword"
+			value="<%=keyword == null ? "" : keyword%>">
+
+		<button type="submit">検索</button>
+
+		<br> <br> <label> <input type="checkbox"
+			name="favoriteOnly" value="1"
+			<%="1".equals(favoriteOnly) ? "checked" : ""%>> お気に入りだけ表示
+		</label>
+	</form>
+
+	<hr>
+
 	<%
 	if (recipeList != null && recipeList.size() > 0) {
 		for (int i = 0; i < recipeList.size(); i++) {
 			Recipe recipe = recipeList.get(i);
 	%>
-	<h2><%=recipe.getRecipeName()%></h2>
-	
-	<% if (recipe.getThumbnailUrl() != null && recipe.getThumbnailUrl().length() > 0) { %>
-    <img src="<%= recipe.getThumbnailUrl() %>" width="250">
-<% } %>
 
-	<p>
-		URL： <a href="<%=recipe.getUrl()%>" target="_blank"> <%=recipe.getUrl()%>
-		</a>
-	</p>
+	<div class="recipe-card">
+		<h2>
+			<%=recipe.getRecipeName()%>
 
-	<p>
-		メモ：
-		<%=recipe.getMemo() == null ? "" : recipe.getMemo()%>
-	</p>
+			<%if (recipe.getFavorite() == 1) {%>
+			⭐
+			<%
+			}
+			%>
 
-	<p>
-		材料：
-		<%=recipe.getMaterials() == null ? "" : recipe.getMaterials()%>
-	</p>
+		</h2>
 
-	<p>
-		作り方：
-		<%=recipe.getSteps() == null ? "" : recipe.getSteps()%>
-	</p>
-	
-	<form action="RecipeEditServlet" method="post">
-		<input type="hidden" name="id" value="<%=recipe.getId()%>">
-		<button type="submit">編集</button>
-	</form>
-	
-	<form action="FavoriteServlet" method="post">
-
-    <input type="hidden"
-           name="id"
-           value="<%= recipe.getId() %>">
-
-<%
-if(recipe.getFavorite() == 0){
-%>
-
-    <input type="hidden"
-           name="favorite"
-           value="1">
-
-    <button type="submit">
-        ☆ お気に入り
-    </button>
-
-<%
-}else{
-%>
-
-    <input type="hidden"
-           name="favorite"
-           value="0">
-
-    <button type="submit">
-        ★ お気に入り解除
-    </button>
-
-<%
+		<%
+		if (recipe.getThumbnailUrl() != null && recipe.getThumbnailUrl().length() > 0) {
+		%>
+		<img src="<%=recipe.getThumbnailUrl()%>" width="250">
+		<%
 }
 %>
 
-</form>
+		<p>
+			<a href="<%=recipe.getUrl()%>" target="_blank"> YouTubeで見る </a>
+		</p>
+
+		<p>
+			メモ：
+			<%=recipe.getMemo() == null ? "" : recipe.getMemo()%>
+		</p>
+
+		<p>
+			材料：
+			<%=recipe.getMaterials() == null
+		? ""
+		: "・" + recipe.getMaterials()
+				.replace("\n", "<br>・")%>
+		</p>
 
 
-	<form action="RecipeDeleteServlet" method="post">
+		<p>
+			作り方：<br>
+			<%=addStepNumbers(recipe.getSteps())%>
+		</p>
 
-		<input type="hidden" name="id" value="<%=recipe.getId()%>">
 
-		<button type="submit">削除</button>
-	</form>
+
+		<form action="RecipeEditServlet" method="post">
+			<input type="hidden" name="id" value="<%=recipe.getId()%>">
+			<button type="submit">編集</button>
+		</form>
+		
+		<form action="RecipeDeleteServlet" method="post">
+			<input type="hidden" name="id" value="<%=recipe.getId()%>">
+			<button type="submit">削除</button>
+		</form>
+
+		<form action="FavoriteServlet" method="post">
+			<input type="hidden" name="id" value="<%=recipe.getId()%>">
+
+			<%
+			if (recipe.getFavorite() == 0) {
+			%>
+			<input type="hidden" name="favorite" value="1">
+			<button type="submit">☆ お気に入り</button>
+			<%
+} else {
+%>
+			<input type="hidden" name="favorite" value="0">
+			<button type="submit">★ お気に入り解除</button>
+			<%
+}
+%>
+		</form>
+
+		
+	</div>
 
 	<hr>
+
 	<%
 	}
 	} else {
@@ -119,5 +190,6 @@ if(recipe.getFavorite() == 0){
 
 	<a href="RecipeAdd.jsp">料理を追加する</a>
 	<a href="index.jsp">トップへ戻る</a>
+
 </body>
 </html>
