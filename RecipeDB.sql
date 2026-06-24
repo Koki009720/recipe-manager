08548838e681b9e9d25cd3cb353db40a405d3f99
Koki009720 <tanakakouki419@gmail.com>
2026-06-24 17:46:12 +0900

    おまけ

--- RecipeDB.sql
+++ RecipeDB.sql
@@ -1,3 +1,7 @@
 ALTER TABLE recipes ADD COLUMN materials TEXT;
 ALTER TABLE recipes ADD COLUMN steps TEXT;
 ALTER TABLE recipes ADD COLUMN thumbnail_url TEXT;
+SELECT recipe_name, thumbnail_url FROM recipes;
+ALTER TABLE recipes ADD COLUMN favorite INTEGER DEFAULT 0;
+ALTER TABLE recipes ADD COLUMN tag TEXT;
+PRAGMA table_info(recipes);
